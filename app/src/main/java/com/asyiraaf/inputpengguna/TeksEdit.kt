5cce841103537a10c79ee8421d5175932fd1470c
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 12:12:33 +0700

    feat: add horizontal divider for visual separation between form sections

--- app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -20,8 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.unit.dp
@@ -74,6 +79,14 @@ fun FormDataDiri(modifier: Modifier){
                     onValueChange = {
                         textAlamat = it
                     }
+                )
+
+                HorizontalDivider(
+                    modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                        id = R.dimen.padding_medium)
+                    ),
+                    thickness = dimensionResource(R.dimen.divider_tipis),
+                    color = Color.DarkGray
                 )
             }
         }
