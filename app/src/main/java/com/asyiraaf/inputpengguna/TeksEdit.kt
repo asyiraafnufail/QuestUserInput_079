5f4f4f09eebc55ff1f8704553ce8134dcddc51a3
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 12:02:38 +0700

    feat: add outlined text field for full address input

--- app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
@@ -66,6 +66,15 @@ fun FormDataDiri(modifier: Modifier){
                     )
                     Text(item)
                 }
+                OutlinedTextField(
+                    value = textAlamat,
+                    singleLine = true,
+                    modifier = Modifier.width(250.dp),
+                    label = {Text(text = "Alamat Lengkap")},
+                    onValueChange = {
+                        textAlamat = it
+                    }
+                )
             }
         }
     }
