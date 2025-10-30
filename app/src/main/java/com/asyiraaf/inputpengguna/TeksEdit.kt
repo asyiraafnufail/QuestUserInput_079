a0d8b57b386b9d821c1d7b8fa28d1ba743273a05
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 11:38:31 +0700

    feat: menambahkan variabel untuk menyimpan nilai input dari keyboard

--- app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
@@ -1,2 +1,16 @@
 package com.asyiraaf.inputpengguna
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier){
+    // variabel untuk mengingat nilai masukan input
+    var textNama by remember { mutableStateOf( "") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+}
