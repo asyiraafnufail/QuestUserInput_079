0a1eee1fa39cfa96cef8ffaa6d9b9f580c547de8
asyiraafnufail <asyiraaf4@gmail.com>
2025-10-30 11:58:32 +0700

    feat: add radiobutton to choose gender

--- app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/asyiraaf/inputpengguna/TeksEdit.kt
@@ -1,13 +1,17 @@
 package com.asyiraaf.inputpengguna
 
+import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
 import androidx.compose.foundation.content.MediaType.Companion.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -49,5 +53,19 @@ fun FormDataDiri(modifier: Modifier){
                 textNama = it
             }
         )
+
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK == item}
+                ), verticalAlignment = Alignment.CenterVertically){
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = { textJK = item}
+                    )
+                }
+            }
+        }
     }
 }
